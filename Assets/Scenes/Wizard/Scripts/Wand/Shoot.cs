ed765fd21e66695392825cd057bf06fc976b000c
Aaron <111891230+AaronPerkey@users.noreply.github.com>
2024-03-20 17:15:54 -0500

    added sword

--- Assets/Scenes/Wizard/Scripts/Wand/Shoot.cs
+++ Assets/Scenes/Wizard/Scripts/Wand/Shoot.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Shoot : MonoBehaviour
 {
@@ -10,23 +11,24 @@ public class Shoot : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Ctrl was pressed, launch a projectile
-        if (Input.GetButtonDown("Fire1"))
-        {
-            Debug.Log(Input.mousePosition);
+
+
+        //if ()
+        //{
+            //Debug.Log("action was accepted");
             // Instantiate the projectile at the position and rotation of this transform
-            Rigidbody clone;
-            clone = Instantiate(projectile, transform.position, transform.rotation);
+            //Rigidbody clone;
+            //clone = Instantiate(projectile, transform.position, transform.rotation);
 
             // Give the cloned object an initial velocity along the current
             // object's Z axis
-            clone.velocity = transform.TransformDirection(Vector3.forward * 10);
-        }
+            //clone.velocity = transform.TransformDirection(Vector3.forward * 10);
+       //}
     }
 }
