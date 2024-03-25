63054cdea9d1662876a53f0ade8778fb80e094d2
Aaron <111891230+AaronPerkey@users.noreply.github.com>
2024-03-24 19:46:37 -0500

    Shooting and sword
    The Shooting finaly works!!! Also, I put the sword in a position for the player to hold.

--- Assets/Scenes/Wizard/Scripts/Wand/Shoot.cs
+++ Assets/Scenes/Wizard/Scripts/Wand/Shoot.cs
@@ -1,34 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class Shoot : MonoBehaviour
 {
+    public GameObject spell;
+    public Transform spawnPoint;
+    public float firespeed = 0;
 
-    public Rigidbody projectile;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        XRGrabInteractable grabbable = GetComponent<XRGrabInteractable>();
+        grabbable.activated.AddListener(FireSpell);
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
 
-        //if ()
-        //{
-            //Debug.Log("action was accepted");
-            // Instantiate the projectile at the position and rotation of this transform
-            //Rigidbody clone;
-            //clone = Instantiate(projectile, transform.position, transform.rotation);
-
-            // Give the cloned object an initial velocity along the current
-            // object's Z axis
-            //clone.velocity = transform.TransformDirection(Vector3.forward * 10);
-       //}
+    public void FireSpell(ActivateEventArgs arg)
+    {
+        GameObject spawnedSpell = Instantiate(spell);
+        spawnedSpell.transform.position = spawnPoint.position;
+        spawnedSpell.GetComponent<Rigidbody>().velocity = spawnPoint.forward * firespeed;
+        Destroy(spawnedSpell, 5);
+        
     }
 }
