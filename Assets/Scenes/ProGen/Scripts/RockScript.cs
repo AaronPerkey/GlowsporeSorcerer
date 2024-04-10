a5e0ad72222128ba4569eb65fa761565c4f8e4fa
Ryan Thomas <rthomas19@unl.edu>
2024-04-10 18:49:35 -0500

    Floor regeneration almost works kinda idk I'm going home and eating food

--- Assets/Scenes/ProGen/Scripts/RockScript.cs
+++ Assets/Scenes/ProGen/Scripts/RockScript.cs
@@ -6,15 +6,12 @@ using UnityEngine.InputSystem.EnhancedTouch;
 
 public class RockScript : MonoBehaviour
 {
-    void Start(){
 
-    }
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Collision detected", other);
+        
         if (other.CompareTag("Hallway"))
         {
-            Debug.Log("destroyed rock");
             // Destroy the rock if it collides with a hallway
             Destroy(gameObject);
         }
