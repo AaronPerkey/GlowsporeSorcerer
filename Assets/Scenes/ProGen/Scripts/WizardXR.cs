bb4d6f9ee742a914dae6908028377f296156f7b8
Ryan Thomas <rthomas19@unl.edu>
2024-04-17 17:08:58 -0500

    fixed player position bug and added ceiling

---
+++ Assets/Scenes/ProGen/Scripts/WizardXR.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WizardXR : MonoBehaviour
+{
+
+    public GameObject xrOrigin;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get the current position of the game object
+        Vector3 currentPosition = transform.position;
+
+        // Update the X and Z coordinates to match xrOrigin
+        currentPosition.x = xrOrigin.transform.position.x;
+        currentPosition.z = xrOrigin.transform.position.z;
+
+        // Update the position of the game object
+        transform.position = currentPosition;
+    }
+}
