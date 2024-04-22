15c908ad638f4d7633765d9257ff2ae263d5336e
Aaron <111891230+AaronPerkey@users.noreply.github.com>
2024-04-22 17:09:28 -0500

    Ai is close to being done

---
+++ Assets/Scenes/Wizard/Scripts/AI Movement/AiIdleState.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiIdleState : AiState
+{
+    public void Enter(AiAgent agent)
+    {
+        Vector3 playerDirection = agent.playerTransform.position - agent.transform.position;
+        if (playerDirection.magnitude > agent.config.maxSightDistance )
+        {
+            return;
+        }
+
+        Vector3 agentDirection = agent.transform.forward;
+        agentDirection.Normalize();
+        float dotProduct = Vector3.Dot(playerDirection, agentDirection);
+        if (dotProduct > 0.0f ) 
+        {
+            agent.stateMachine.ChangeState(AiStateId.ChasePlayer);
+        }
+    }
+
+    public void Exit(AiAgent agent)
+    {
+    }
+
+    public AiStateId GetId()
+    {
+        return AiStateId.Idle;
+    }
+
+    public void Update(AiAgent agent)
+    {
+    }
+}
