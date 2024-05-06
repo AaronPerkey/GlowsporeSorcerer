a48aff62530c944f97751284824347bee65f80f1
Aaron <aperkey2@huskers.unl.edu>
2024-05-06 11:28:08 -0500

    coin

--- Assets/Scenes/Wizard/Scripts/Enemy Drops/Coin Dropper.cs
+++ Assets/Scenes/Wizard/Scripts/Enemy Drops/Coin Dropper.cs
@@ -1,28 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.ExceptionServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CoinDropper : MonoBehaviour
 {
+    public Transform enemy;
+    public Rigidbody coin;
     private int changceToDrop;
     private int numberOfCoins;
-    //private static readonly Random getrandom = new Random();
+    private float m_Thrust = 20f;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        coin = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public int Amount()
     {
-        
+        int coinAmount;
+        if (Random.Range(0, 1) == 1)
+        {
+            coinAmount = Random.Range(0, 5);
+        }
+        else
+        {
+            coinAmount = 0;
+        }
+
+        return coinAmount;
     }
 
-    //public GameObject Dropcoins()
-    //{
-       //changceToDrop = 
-    //}
+    public void CoinSpawner()
+    {
+        Instantiate(coin, enemy.transform.position, Quaternion.identity);
+        coin.AddForce(transform.up * m_Thrust);
+    }
+
+    public void CoinDrop()
+    {
+        int numberOfCoins = Amount();
+        for (int i = 0; i < numberOfCoins; i++)
+        {
+            CoinSpawner();
+        }
+
+    }
 }
