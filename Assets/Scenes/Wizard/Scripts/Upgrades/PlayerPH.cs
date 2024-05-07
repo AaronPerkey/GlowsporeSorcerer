19950b8c94b1ae8a59b1a6e66cd25ed165d848ff
Aaron <111891230+AaronPerkey@users.noreply.github.com>
2024-05-07 01:02:33 -0500

    late night changes

---
+++ Assets/Scenes/Wizard/Scripts/Upgrades/PlayerPH.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPH : MonoBehaviour
+{
+    PZapSpell zapSpell;
+    PFireSpell fireSpell;
+    PFreezeSpell freezeSpell;
+    Health health;
+
+
+    private void Awake()
+    {
+        zapSpell = GetComponent<ZapSpell>();
+        fireSpell = GetComponent<FireSpell>();
+        freezeSpell = GetComponent<PFreezeSpell>();
+        health = GetComponent<Health>();
+
+        //set inital damage
+        zapSpell.damage = 5;
+        fireSpell.damage = 1;
+        freezeSpell.damage = 10;
+    }
+    public void IncreasePlayerDamage()
+    {
+        zapSpell.damage += 2;
+        fireSpell.damage += 2;
+        freezeSpell.damage += 2;
+    }
+
+    public void IncreasePlayerHealth()
+    {
+        health.maxHealth += 10;
+    }
+}
